4c00e2e79c5a294d3d688a0576f53e7b2503b6c1
swapnil <sopiswarrinku77@gmail.com>
2022-04-26 13:42:11 -0700

    Added : added person details and can search this details

--- addressBook.java
+++ addressBook.java
@@ -22,11 +22,61 @@ class PersonInfo {
         JOptionPane.showMessageDialog(null,"First Name: " + First_Name +"\nLast Name : " + Last_Name +"\nAddress: " + Address + "\nPhone no: " + PhoneNumber + "\nstate : " + State + "\ncity : " + City);
     }
 }
+class AddressBook {
+    ArrayList persons;
 
+    AddressBook() {
+        persons = new ArrayList();
+    }
 
-public class addressBook {
-    public static void main(String[] args) {
-        PersonInfo ab = new PersonInfo(null,null,null,null,null,null);
-        ab.display();
+    // adding a Person Object
+    void addPerson() {
+        String Fname = JOptionPane.showInputDialog("Enter first Name:");
+        String Lname = JOptionPane.showInputDialog("Enter last name:");
+        String Add = JOptionPane.showInputDialog("Enter Address:");
+        String pNum = JOptionPane.showInputDialog("Enter PhoneNo:");
+        String State = JOptionPane.showInputDialog("Enter state:");
+        String City = JOptionPane.showInputDialog("Enter City:");
+
+        // creating a personInfo object
+        PersonInfo p = new PersonInfo(Fname, Lname, Add, pNum, State, City);
+        persons.add(p);
+    }
+
+    // searching for a person
+    void searchPerson(String n) {
+        for (int i = 0; i < persons.size(); i++) {
+            PersonInfo p = (PersonInfo) persons.get(i);
+            if (n.equals(p.First_Name)) {
+                p.display();
+            }
+        }
     }
 }
+
+       public class addressBook {
+
+            public static void main(String[] args) {
+                AddressBook ab = new AddressBook();
+                String input, s;
+                int ch;
+
+                while (true) {
+                    input = JOptionPane.showInputDialog("Enter 1 to Add\nEnter 2 to Search\nEnter 3 to Exit");
+                    ch = Integer.parseInt(input);
+
+
+                    switch (ch) {
+                        case 1:
+                            ab.addPerson();
+                            break;
+                        case 2:
+                            s = JOptionPane.showInputDialog("Enter name to search:");
+                            ab.searchPerson(s);
+                            break;
+                        case 3:
+                            System.exit(0);
+                    }
+                }
+            }
+        }
